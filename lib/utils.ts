5208d510693ef405a357ba6f735a4e169bfdc760
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-08-14 14:14:20 +0200

    refactor: Bug fix and create new utils
    Changes:
    - Refactor the appwrite.config.ts file
    - Create a new utils functions
    - Add new types and validation functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,89 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
 
+// == Classname Utility
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+// == Stringify and Parse Utility
+export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+// == Convert File to URL
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+// == Date Time Formatter
+export const formatDateTime = (dateString: Date | string) => {
+    // Date Time Options
+    const dateTimeOptions: Intl.DateTimeFormatOptions = {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    };
+
+    // Date Day Options
+    const dateDayOptions: Intl.DateTimeFormatOptions = {
+        weekday: "short",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+    };
+
+    // Date Options
+    const dateOptions: Intl.DateTimeFormatOptions = {
+        month: "short",
+        year: "numeric",
+        day: "numeric",
+    };
+
+    // Time Options
+    const timeOptions: Intl.DateTimeFormatOptions = {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    };
+
+    // Formatted Date Time
+    const formattedDateTime: string = new Date(dateString).toLocaleString(
+        "en-US",
+        dateTimeOptions
+    );
+
+    // Formatted Date Day
+    const formattedDateDay: string = new Date(dateString).toLocaleString(
+        "en-US",
+        dateDayOptions
+    );
+
+    // Formatted Date
+    const formattedDate: string = new Date(dateString).toLocaleString(
+        "en-US",
+        dateOptions
+    );
+
+    // Formatted Time
+    const formattedTime: string = new Date(dateString).toLocaleString(
+        "en-US",
+        timeOptions
+    );
+
+    // Return
+    return {
+        dateTime: formattedDateTime,
+        dateDay: formattedDateDay,
+        dateOnly: formattedDate,
+        timeOnly: formattedTime,
+    };
+};
+
+// == Encrypt and Decrypt Key
+export function encryptKey(passkey: string) {
+    return btoa(passkey);
+}
+
+export function decryptKey(passkey: string) {
+    return atob(passkey);
 }
--- types/types.ts
+++ types/types.ts
@@ -1,5 +1,17 @@
+import { Models } from "node-appwrite";
 import { Control } from "react-hook-form";
 
+// ==== Search Params
+export interface SearchParamsProps {
+    params: { [key: string]: string };
+    searchParams: { [key: string]: string | string[] | undefined };
+}
+
+// ==== Main types
+declare type Gender = "Male" | "Female" | "Other";
+declare type Status = "pending" | "scheduled" | "cancelled";
+
+// ==== Form Field
 export enum FormFieldTypes {
     INPUT = "input",
     SELECT = "select",
@@ -30,9 +42,90 @@ export interface RenderFieldProps {
     props: CustomFormFieldProps;
 }
 
+// ==== Submit Button
 export interface SubmitButtonProps {
     isLoading: boolean;
     children: React.ReactNode;
     className?: string;
-    
+}
+
+// ==== Actions
+// == Patient Actions
+export interface Patient extends Models.Document {
+    userId: string;
+    name: string;
+    email: string;
+    phone: string;
+    birthDate: Date;
+    gender: Gender;
+    address: string;
+    occupation: string;
+    emergencyContactName: string;
+    emergencyContactNumber: string;
+    primaryPhysician: string;
+    insuranceProvider: string;
+    insurancePolicyNumber: string;
+    allergies: string | undefined;
+    currentMedication: string | undefined;
+    familyMedicalHistory: string | undefined;
+    pastMedicalHistory: string | undefined;
+    identificationType: string | undefined;
+    identificationNumber: string | undefined;
+    identificationDocument: FormData | undefined;
+    privacyConsent: boolean;
+}
+export interface CreateUserProps {
+    name: string;
+    email: string;
+    phone: string;
+}
+export interface User extends CreateUserProps {
+    $id: string;
+}
+export interface RegisterUserProps {
+    userId: string;
+    birthDate: string;
+    gender: Gender;
+    address: string;
+    occupation: string;
+    emergencyContactName: string;
+    emergencyContactNumber: string;
+    primaryPhysician: string;
+    insuranceProvider: string;
+    insurancePolicyNumber: string;
+    allergies: string | undefined;
+    currentMedication: string | undefined;
+    familyMedicalHistory: string | undefined;
+    pastMedicalHistory: string | undefined;
+    identificationType: string | undefined;
+    identificationNumber: string | undefined;
+    identificationDocument: FormData | undefined;
+    privacyConsent: boolean;
+}
+
+// == Appointment Actions
+export interface Appointment {
+    patient: Patient;
+    schedule: Date;
+    status: Status;
+    primaryPhysician: string;
+    reason: string;
+    note: string;
+    userId: string;
+    cancellationReason: string | null;
+}
+export interface CreateAppointmentProps {
+    userId: string;
+    patient: string;
+    primaryPhysician: string;
+    reason: string;
+    schedule: Date;
+    status: Status;
+    note: string | undefined;
+}
+export interface UpdateAppointmentProps {
+    appointmentId: string;
+    userId: string;
+    appointment: Appointment;
+    type: string;
 }
