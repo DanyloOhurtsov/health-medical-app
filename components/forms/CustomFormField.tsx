974520290790499687e9c55f1ccfd66b72b8d577
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-08-15 17:40:04 +0200

    func: Create GENDER, SKELETON, DATE_PICKER cases
    Changes:
    - Create new cases for CustomFormField component
    - Install date-fns

--- components/forms/CustomFormField.tsx
+++ components/forms/CustomFormField.tsx
@@ -2,12 +2,12 @@
 
 import {
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
+import { format } from "date-fns";
 import { Input } from "../ui/input";
 import {
     CustomFormFieldProps,
@@ -15,12 +15,31 @@ import {
     RenderFieldProps,
 } from "@/types/types";
 import Image from "next/image";
-// import { PhoneInput } from "../ui/phone-input";
 import { E164Number } from "libphonenumber-js";
 import { PhoneInput } from "../ui/phone-input";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { Calendar } from "../ui/calendar";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "../ui/select";
+import { GenderOptions } from "@/lib/constants";
 
 const RenderField = ({ field, props }: RenderFieldProps) => {
-    const { fieldType, iconAlt, iconSrc, placeholder } = props;
+    const {
+        fieldType,
+        iconAlt,
+        iconSrc,
+        placeholder,
+        dateFormat,
+        showTimeSelect,
+        renderSkeleton,
+    } = props;
 
     switch (fieldType) {
         case FormFieldTypes.INPUT:
@@ -66,6 +85,75 @@ const RenderField = ({ field, props }: RenderFieldProps) => {
                     </FormControl>
                 </div>
             );
+        case FormFieldTypes.DATE_PICKER:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400 min-h-14 items-center">
+                    <Image
+                        src={"/assets/icons/calendar.svg"}
+                        alt="cal"
+                        height={24}
+                        width={24}
+                        className="ml-2 w-auto h-auto"
+                    />
+                    <FormControl>
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    variant={"ghost"}
+                                    className={cn(
+                                        "w-full justify-start text-left font-normal",
+                                        !field.value && "text-muted-foreground"
+                                    )}
+                                >
+                                    {field.value ? (
+                                        format(
+                                            field.value,
+                                            `${dateFormat ?? "dd/MM/yyyy"}`
+                                        )
+                                    ) : (
+                                        <span className="text-muted-foreground">
+                                            Pick a date
+                                        </span>
+                                    )}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-auto p-0">
+                                <Calendar
+                                    mode="single"
+                                    selected={field.value}
+                                    onSelect={(date: any) =>
+                                        field.onChange(date)
+                                    }
+                                    initialFocus
+                                    className="bg-dark-400 border-dark-500"
+                                    classNames={{ root: "border-dark-500" }}
+                                />
+                            </PopoverContent>
+                        </Popover>
+                    </FormControl>
+                </div>
+            );
+        case FormFieldTypes.SKELETON:
+            return renderSkeleton ? renderSkeleton(field) : null;
+        case FormFieldTypes.GENDER:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400 min-h-14 items-center">
+                    <FormControl>
+                        <Select>
+                            <SelectTrigger className="w-full focus:ring-0 focus:border-none focus:outline-none focus:ring-offset-0 ring-0 border-0 outline-none ring-offset-0">
+                                <SelectValue placeholder="---" className="text-dark-500"/>
+                            </SelectTrigger>
+                            <SelectContent className="bg-dark-400 border-dark-500 w-64 -ml-2.5">
+                                {GenderOptions.map((option) => (
+                                    <SelectItem value={option.id} className="cursor-pointer focus:bg-dark-500 transition-all rounded-md">
+                                        {option.label}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </FormControl>
+                </div>
+            );
         default:
             break;
     }
@@ -85,7 +173,7 @@ const CustomFormField = (props: CustomFormFieldProps) => {
                         <FormLabel>{label}</FormLabel>
                     )}
 
-                    <RenderField field={field} props={props}/>
+                    <RenderField field={field} props={props} />
 
                     <FormMessage className="shad-error" />
                 </FormItem>
