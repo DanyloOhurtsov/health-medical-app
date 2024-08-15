338dd5e80db2b3c9d4d2389167934eae253957b3
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-08-15 17:38:39 +0200

    func: Create getUser action

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -5,6 +5,7 @@ import { users } from "../appwrite.config";
 import { ID, Query } from "node-appwrite";
 import { parseStringify } from "../utils";
 
+// == Create a new user
 export const createUser = async (user: CreateUserProps) => {
     try {
         const newUser = await users.create(
@@ -25,5 +26,16 @@ export const createUser = async (user: CreateUserProps) => {
             return existingUser?.users[0];
         }
         throw error;
+    }
+};
+
+// == Get a user by ID
+export const getUser = async (userId: string) => {
+    try {
+        const user = await users.get(userId);
+
+        return parseStringify(user);
+    } catch (error) {
+        console.error(error);
     }
 };
